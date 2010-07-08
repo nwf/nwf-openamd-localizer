4bc2b28af2cba02bc1a117d9c969b05be794aee0
Nathaniel Wesley Filardo <nwf@pf.priv.oc.ietfng.org>
2010-07-07 20:47:32 -0400

    Better sputnik_to_norm.pl framing

--- sputnik_to_norm.pl
+++ sputnik_to_norm.pl
@@ -1,37 +1,53 @@
 #!/usr/bin/env perl
+#
+# This script converts sputnik-style binary log files into
+# the normalized packet format used for testing the framework
+#
+# Those log files are binary dumps of network-endian packed
+# structures, containing, in order:
+# 	time (seconds, 4 bytes)
+#	IPv4 address (4 bytes)
+#   size (OpenAMD frame, 1 byte)
+#		size - 3 bytes (OpenAMD packet data, fields in network endianness)
+#   CRC16 (OpenAMD frame, really CRCCCITT, 2 bytes)
+#
+# This script duplicates framing logic but doesn't do a whole
+# lot of checking before handing it off as a normalized line.
+# It knows enough to do CRC checking if you want, but by default,
+# to keep dependencies down, it's commented out
+
 use strict;
 use warnings;
 
-use Digest::CRC qw(crcccitt crc16) ;
+# use Digest::CRC qw(crcccitt crc16) ;
 
-                 #  T 
-                 #  I 
-                 #  MI
-                 #  EP
-                 #  44
-my $PKT_HDR_FMT  = "NN";
-                 #  S F   R
-                 #  IPLSSOE
-                 #  ZRATEIS
-                 #  EOGRQDV
-                 #  1111442
-my $PKT_DATA     = "CCCCNNn";
-
-my $PKT_SIZE = 24;
+                 #  T S 
+                 #  I I
+                 #  MIZ
+                 #  EPE
+                 #  441
+my $PKT_HDR_FMT  = "NNC";
+my $PKT_HDR_SIZE = 9;
 
 my $fn = shift @ARGV;
 my $fh;
 open $fh, "<$fn" or die "Can't open '$fn' for reading: '$!'";
 
-my $pkt;
-while((read $fh, $pkt, $PKT_SIZE) == $PKT_SIZE) {
-	my ($time, $ip) = unpack $PKT_HDR_FMT, (substr $pkt, 0, 8);
-	my $data = substr $pkt, 8, 14;
-	my $crc = substr $pkt, 22, 2;
+my $len;
+my $hdr;
+while(($len = read $fh, $hdr, $PKT_HDR_SIZE) == $PKT_HDR_SIZE) {
+	my ($time, $ip, $size) = unpack $PKT_HDR_FMT, $hdr;
 
-	die if(crcccitt($data) != unpack "n", $crc);
+	my $pkt;
+	((read $fh, $pkt, ($size - 1)) == ($size - 1)) or die "Payload read: $!";
 
-    printf "%08X %016X 00000 ", $ip, $time;
+	my $data = substr $pkt, 0, $size-3;
+	my $crc = substr $pkt, $size-3, 2;
+	# die if(crcccitt((chr $size).$data) != unpack "n", $crc);
+
+    printf "%08X %016X 00000 %02X", $ip, $time, $size;
     map { printf "%02X", ord $_ } split //, ($data.$crc);
 	printf "\n";
 }
+
+die "Header read: $!" if not defined $len;
